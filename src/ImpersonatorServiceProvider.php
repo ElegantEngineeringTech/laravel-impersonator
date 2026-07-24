c0cf8d9084f06c444d3c35741944d517d3cdf40e
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-07-24 16:34:55 +0200

    add blade directive

--- src/ImpersonatorServiceProvider.php
+++ src/ImpersonatorServiceProvider.php
@@ -4,6 +4,7 @@ namespace Elegantly\Impersonator;
 
 use Elegantly\Impersonator\Facades\Impersonator;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -24,6 +25,10 @@ class ImpersonatorServiceProvider extends PackageServiceProvider
 
     public function packageBooted(): void
     {
+        Blade::if('impersonating', function (mixed $condition = true) {
+            return Impersonator::isImpersonating() && value($condition);
+        });
+
         Gate::define('impersonate', static function (?Authenticatable $auth, ?Authenticatable $user) {
 
             if ($auth === null || $user === null) {
