fdddc6a970f2b447c3faa82c553dc06eda7a8758
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-07-24 15:20:17 +0200

    doc

--- src/Facades/Impersonator.php
+++ src/Facades/Impersonator.php
@@ -5,6 +5,12 @@ namespace Elegantly\Impersonator\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Illuminate\Contracts\Auth\Authenticatable|null getImpersonator()
+ * @method static int|null getImpersonatorId()
+ * @method static bool isImpersonating()
+ * @method static void take(null|int|\Illuminate\Contracts\Auth\Authenticatable $user)
+ * @method static void leave()
+ *
  * @see \Elegantly\Impersonator\Impersonator
  */
 class Impersonator extends Facade
