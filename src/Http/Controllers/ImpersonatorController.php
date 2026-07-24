50e5ea483407f74adf4ea1038c858989474a9aa9
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-07-24 18:26:21 +0200

    fix

--- src/Http/Controllers/ImpersonatorController.php
+++ src/Http/Controllers/ImpersonatorController.php
@@ -2,9 +2,7 @@
 
 namespace Elegantly\Impersonator\Http\Controllers;
 
-use Elegantly\Impersonator\Contracts\Impersonate;
 use Elegantly\Impersonator\Facades\Impersonator;
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -13,7 +11,6 @@ class ImpersonatorController extends Controller
 {
     public function take(string|int $user): RedirectResponse
     {
-        /** @var null|Authenticatable&Impersonate */
         $impersonated = Auth::getProvider()->retrieveById($user);
 
         Gate::authorize('impersonate', $impersonated);
