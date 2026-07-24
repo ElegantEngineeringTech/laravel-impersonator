0db9e5b5c760e0e1647a79d62b6f563ddb44ef3c
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-07-24 18:25:30 +0200

    redirect intended

--- src/Http/Controllers/ImpersonatorController.php
+++ src/Http/Controllers/ImpersonatorController.php
@@ -2,7 +2,9 @@
 
 namespace Elegantly\Impersonator\Http\Controllers;
 
+use Elegantly\Impersonator\Contracts\Impersonate;
 use Elegantly\Impersonator\Facades\Impersonator;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -11,19 +13,24 @@ class ImpersonatorController extends Controller
 {
     public function take(string|int $user): RedirectResponse
     {
+        /** @var null|Authenticatable&Impersonate */
         $impersonated = Auth::getProvider()->retrieveById($user);
 
         Gate::authorize('impersonate', $impersonated);
 
         Impersonator::take($impersonated);
 
-        return redirect()->back();
+        return redirect()->intended(
+            url()->previous('/')
+        );
     }
 
     public function leave(): RedirectResponse
     {
         Impersonator::leave();
 
-        return redirect()->back();
+        return redirect()->intended(
+            url()->previous('/')
+        );
     }
 }
